1670b410136941e95bceb8ae3f7e2794adefc7bb
EstefaniaGav <124322868+EstefaniaGav@users.noreply.github.com>
2023-12-18 13:30:17 -0500

    ola

--- src/Pages/newPurchase.jsx
+++ src/Pages/newPurchase.jsx
@@ -145,7 +145,7 @@ function NewPurchase() {
 
   };
 
-  const { getSupplies } = useSupplies()
+  const {  getShopSupplies } = useSupplies()
 
   const updateTotalValue = (array = selectedSupplies) => {
     setShoppingBillState(prev => ({
@@ -154,14 +154,14 @@ function NewPurchase() {
     }))
   }
   useEffect(() => {
-    setSuppliesState(getSupplies())
+    setSuppliesState( getShopSupplies())
     // console.log("Supplies")
     updateTotalValue()
     return async () => {
-      const newSupplies = await Promise.resolve(getSupplies())
+      const newSupplies = await Promise.resolve( getShopSupplies())
       setSuppliesState(newSupplies)
     }
-    // console.log(getSupplies())
+    console.log( getShopSupplies())
   }, [])
 
   /**
